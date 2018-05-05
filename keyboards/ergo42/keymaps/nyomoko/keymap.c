1a92c343bede7960a23f415d7c962b4766abdbeb
nyomo <nyomoko@gmail.com>
2018-05-06 01:05:01 +0900

    BASEのキー配列を設定

--- keyboards/ergo42/keymaps/nyomoko/keymap.c
+++ keyboards/ergo42/keymaps/nyomoko/keymap.c
@@ -17,22 +17,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  @   |
+ * | GUI  |  ESC |   Q  |   W  |   E  |   R  |  T   |   |   Y  |   U  |   I  |   O  |   P  |  @   |Del/BS|
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
+ * | Tab  | Ctrl |   A  |   S  |   D  |   F  |  G   |   |   H  |   J  |   K  |   L  |   ;  |  :   |Return|
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
+ * |=>META| Shift|   Z  |   X  |   C  |   V  |   B  |   |  N   |   M  |   ,  |   .  |   /  |  \   |  -   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |PrtSc |=>GAME|=>SYMB|  \   |
- * |      |      |      |      |~SYMB |RCtrl |Shift |   |Space |~META |      |      |      |      |      |
+ * |   1  |   2  |   3  |   4  |   5  |  Alt | Spc  |   | XFER |   6  |   7  |   8  |   9  |   0  |  =   |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,             KC_T,          KC_RBRC,       KC_BSLS,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_LBRC, \
-  KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,             KC_G,          S(KC_8),       S(KC_9),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,             KC_B,          S(KC_RBRC),    S(KC_BSLS), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_RO), \
-  KC_LCTRL, KC_LGUI, KC_APP, KC_PSCR, LT(SYMB, KC_ESC), RCTL_T(KC_SPC), SFT_T(KC_TAB), KC_BSPC,    LT(META, KC_ENT), KC_DELT, KC_PSCR,  TG(GAME), TG(SYMB), KC_JYEN \
+  KC_LGUI ,KC_ESC    ,KC_Q, KC_W, KC_E, KC_R  , KC_T  ,KC_Y   , KC_U, KC_I,     KC_O,     KC_P,    KC_LBRC,KC_BSPC ,\
+  KC_LALT ,KC_LCTRL ,KC_A, KC_S, KC_D, KC_F  , KC_G  ,KC_H   , KC_J, KC_K,     KC_L,     KC_SCLN,  KC_QUOT,KC_ENT ,\
+  MO(META),KC_LSFT , KC_Z, KC_X, KC_C, KC_V  , KC_B  ,KC_N   , KC_M, KC_COMM,  KC_DOT,   KC_SLSH,  KC_JYEN,KC_MINS ,\
+  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_LALT,KC_SPC,KC_HENK,KC_6,  KC_7 ,    KC_8,     KC_9,     KC_0,   KC_LBRC \
 ),
 
 /* META
