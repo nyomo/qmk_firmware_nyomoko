5aa79c10923938750be23887789f4e4d520e81ee
nyomo <nyomoko@gmail.com>
2018-06-04 09:33:35 +0900

    アルファベットキーだけ入れ替えてみた

--- keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
+++ keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
@@ -13,6 +13,7 @@
 #ifdef SSD1306OLED
   #include "ssd1306.h"
 #endif
+#include "keymap_extras/sendstring_jis.h"
 
 extern keymap_config_t keymap_config;
 
@@ -77,10 +78,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   		
   [_QWERTY] = KEYMAP( \
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-      ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_EQL, KC_ENT, \
-      KC_LSFT, KC_JYEN, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,     KC_SCLN, KC_QUOT, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH , \
+      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_BSPC,                    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
+      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSLS,                    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_EQL, KC_ENT,                     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
+      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,S(KC_RO),  KC_LSFT, KC_LCTL, KC_ASTR, KC_Z,    KC_X,    KC_C,   KC_V, \
       KC_LCTRL,XXXXXXX,XXXXXXX ,XXXXXXX,  KC_LALT,KC_SPC,KC_LGUI,    XXXXXXX,KC_HENK,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),S(KC_RO)\
       ),
 
