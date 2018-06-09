7993bc719ff2718b2b2cdae5040b0379951abb17
nyomo <nyomoko@gmail.com>
2018-06-10 00:15:19 +0900

    分割キーボードとして使う用に再調整

--- keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
+++ keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
@@ -13,7 +13,7 @@
 #ifdef SSD1306OLED
   #include "ssd1306.h"
 #endif
-#include "keymap_extras/sendstring_jis.h"
+#include "keymap_extras/keymap_jp.h"
 
 extern keymap_config_t keymap_config;
 
@@ -78,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   		
   [_QWERTY] = KEYMAP( \
-      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_BSPC,                    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
-      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSLS,                    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
-      KC_H,    KC_J,    KC_K,    KC_L,    KC_EQL, KC_ENT,                     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
-      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,S(KC_RO),  KC_LCTL, KC_LSFT, KC_ASTR, KC_Z,    KC_X,    KC_C,   KC_V, \
-      KC_HENK,XXXXXXX,KC_LGUI,XXXXXXX,  KC_LALT,KC_SPC,XXXXXXX,    XXXXXXX,ADJUST,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),KC_SPC\
+      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_BSPC,                     KC_ESC,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
+      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,                      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,                     ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
+      KC_INS,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,JP_YEN,     KC_LCTL, KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,   KC_B, \
+      KC_HENK, XXXXXXX, KC_LGUI, JP_LBRC, JP_RBRC,JP_CIRC,S(KC_RO),   KC_RALT, KC_1    ,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),KC_SPC\
       ),
 
   /* Adjust (Lower + Raise)
@@ -99,35 +99,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_ADJUST] =  KEYMAP( \
-      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_DEL, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______,_______ , KC_DEL, \
-      _______, AG_SWAP, _______, AU_ON,   AU_OFF,  AG_NORM,                   KC_LEFT, KC_DOWN, KC_UP,KC_RIGHT,  _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, RGB_M_R, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, RGB_M_P, RGB_M_B,RGB_M_X, RGB_M_SW, RGB_M_R,RGB_M_SN, RGB_M_K, _______,RGB_M_G, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD \
-      ),
-
-  /* Lower
-   * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  |      |
-   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |   _  |   +  |   {  |   }  |  |   |
-   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  (   |   )  |  F12 |      |      | Home | End  |      |
-   * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
-   * `-------------------------------------------------------------------------------------------------'
-   */
-  [_LOWER] = KEYMAP( \
-      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LPRN, KC_RPRN, KC_F12,  _______, _______, KC_HOME, KC_END,  _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
-      )
-
-
+      KC_F7  , KC_F8,   KC_F9,   KC_F10,  KC_F4,  KC_DEL,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  _______  ,JP_COLN,                   _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,XXXXXXX,\
+      _______, KC_PGDN, KC_PGUP, RGB_HUD, RGB_HUI, RGB_VAI,RGB_TOG ,     RGBRST, _______, RGB_M_R, RGB_M_X, RGB_M_G, RGB_M_K, RGB_M_SN,\
+      _______, RGB_M_P, AG_SWAP, RGB_SAD, RGB_SAI, RGB_VAD,RGB_MOD ,     RESET , _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+  )
 };
 
 #elif HELIX_ROWS == 4
