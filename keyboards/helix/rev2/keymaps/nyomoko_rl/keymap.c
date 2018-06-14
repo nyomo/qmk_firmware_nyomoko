ff44177e08b20e5affc5c792d8fc6aa6b9212e30
nyomo <tomoko.ymda@pepabo.com>
2018-06-14 20:57:34 +0900

    やっぱり数字は並んでた方が良い

--- keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
+++ keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
@@ -78,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   		
   [_QWERTY] = KEYMAP( \
-      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_BSPC,                     KC_ESC,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
-      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,                      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
+      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_BSPC,                     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
+      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,                      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,                     ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
       KC_INS,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,JP_YEN,     KC_LCTL, KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,   KC_B, \
-      KC_HENK, XXXXXXX, KC_LGUI, JP_LBRC, JP_RBRC,JP_CIRC,S(KC_RO),   KC_RALT, KC_1    ,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),KC_SPC\
+      KC_HENK, XXXXXXX, KC_LALT, JP_LBRC, JP_RBRC,JP_CIRC,S(KC_RO),   KC_LGUI, KC_TAB    ,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),KC_SPC\
       ),
 
   /* Adjust (Lower + Raise)
