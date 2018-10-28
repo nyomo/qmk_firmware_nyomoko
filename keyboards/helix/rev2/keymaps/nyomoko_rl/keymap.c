49bd9caf02fec0dde72995835c95b3507c9e3ca1
nyomo <nyomoko@gmail.com>
2018-10-29 00:54:11 +0900

    右手の配置と左手のFNの位置を変えた

--- keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
+++ keyboards/helix/rev2/keymaps/nyomoko_rl/keymap.c
@@ -76,9 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
       KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, JP_CIRC,                     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6, \
       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    JP_AT,                      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, \
-      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN,                     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
-      JP_YEN,  KC_N,    KC_M,    KC_COMM, KC_DOT , KC_SLSH,KC_ENT,     KC_LCTL, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B, \
-      KC_HENK, XXXXXXX, KC_LALT, JP_LBRC, JP_RBRC, KC_BSPC,S(KC_RO),   KC_LGUI, ADJUST,KC_EQL,KC_LBRC,S(KC_RBRC),S(KC_BSLS),KC_SPC\
+      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,                     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G, \
+      KC_BSPC,  KC_N,    KC_M,    KC_COMM, KC_DOT , KC_SLSH,JP_COLN,     ADJUST, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B, \
+      KC_HENK, KC_BSPC, KC_LALT, JP_LBRC, JP_RBRC, JP_YEN ,S(KC_RO),   KC_LGUI, KC_LCTL,KC_EQL,KC_LBRC,S(KC_RBRC),KC_LGUI,KC_SPC\
       ),
 
 
@@ -179,13 +179,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      | MODE | HUE- | SAT- | VAL- |
    * `-------------------------------------------------------------------------------------------------'
    */
+
   [_ADJUST] =  LAYOUT( \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
-      )
+      KC_F7  , KC_F8,   KC_F9,   KC_F10,  KC_F4,  KC_DEL,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,JP_COLN,                     _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,XXXXXXX,\
+      _______, KC_PGDN, KC_PGUP, RGB_HUD, RGB_HUI, RGB_VAI,RGB_TOG ,   _______, _______, RGB_M_R, RGB_M_X, RGB_M_G, RGB_M_K, RGB_M_SN,\
+      _______, RGB_M_P, AG_SWAP, RGB_SAD, RGB_SAI, RGB_VAD,RGB_MOD ,   RESET , _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+  )
+
 };
 
 #elif HELIX_ROWS == 4
@@ -293,11 +295,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      | MODE | HUE- | SAT- | VAL- |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_ADJUST] =  LAYOUT( \
-      _______, RESET,   _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SMOD,RGB_HUD, RGB_SAD, RGB_VAD \
+  [_ADJUST] =  KEYMAP( \
+      KC_F7  , KC_F8,   KC_F9,   KC_F10,  KC_F4,  KC_DEL,                      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, \
+      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+      KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, _______,JP_COLN,                     _______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,XXXXXXX,\
+      _______, KC_PGDN, KC_PGUP, RGB_HUD, RGB_HUI, RGB_VAI,RGB_TOG ,   RGBRST, _______, RGB_M_R, RGB_M_X, RGB_M_G, RGB_M_K, RGB_M_SN,\
+      _______, RGB_M_P, AG_SWAP, RGB_SAD, RGB_SAI, RGB_VAD,RGB_MOD ,   RESET , _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
       )
 };
 
