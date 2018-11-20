2f45d624364c28447b08684c2d9c287f1aec1a06
nyomo <nyomoko@gmail.com>
2018-11-21 08:12:00 +0900

    helixのキーマップに似せてみた

--- keyboards/ergo42/keymaps/nyomoko/keymap.c
+++ keyboards/ergo42/keymaps/nyomoko/keymap.c
@@ -1,6 +1,7 @@
 #include "ergo42.h"
 #include "action_layer.h"
 #include "eeconfig.h"
+#include "keymap_extras/keymap_jp.h"
 
 extern keymap_config_t keymap_config;
 
@@ -26,10 +27,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [BASE] = KEYMAP( \
-  KC_ESC,KC_LGUI, KC_Q, KC_W, KC_E, KC_R   , KC_T  ,KC_Y   , KC_U, KC_I,     KC_O,     KC_P,    KC_MINS ,KC_BSPC ,\
-  KC_TAB,MO(META), KC_A, KC_S, KC_D, KC_F   , KC_G  ,KC_H   , KC_J, KC_K,     KC_L,     KC_SCLN, KC_QUOT,KC_ENT ,\
-  KC_LALT,KC_LSFT  , KC_Z, KC_X, KC_C, KC_V   , KC_B  ,KC_B   , KC_N, KC_M,     KC_COMM,  KC_DOT,   KC_SLSH, KC_JYEN,\
-  KC_LCTRL,KC_1,KC_2, KC_3, KC_4, KC_5, KC_SPC ,KC_HENK, KC_6, KC_7 ,    KC_8,     KC_9,    KC_0,   KC_LBRC \
+  KC_LGUI,KC_ESC, KC_Q, KC_W, KC_E, KC_R   , KC_T  ,KC_Y   , KC_U, KC_I, KC_O, KC_P, KC_MINS ,KC_BSPC ,\
+  KC_TAB,MO(META), KC_A, KC_S, KC_D, KC_F, KC_G  ,KC_H , KC_J, KC_K,   KC_L,KC_SCLN,JP_COLN,KC_ENT ,\
+  KC_LALT,KC_LSFT  , KC_Z, KC_X, KC_C, KC_V   , KC_B  ,KC_B   , KC_N, KC_M,     KC_COMM,  KC_DOT, KC_SLSH, JP_YEN,\
+  KC_LCTRL,KC_1,KC_2, KC_3, KC_4, KC_5, KC_SPC ,KC_HENK, KC_6, KC_7 ,    KC_8,     KC_9,    KC_0,  JP_AT \
 ),
 
 /* META
